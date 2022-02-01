c4ada68fa10750b58d0675ba93a3dd6dd8a53490
Luis Monte <luismonte.l560@gmail.com>
2022-01-31 22:53:37 -0300

    2. Teste o componente <About.js />.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('2. Teste o componente <About.js />.', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const about = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(about).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const pokemonText = screen.getAllByText(/Pokémons/i);
+    expect(pokemonText).toHaveLength(2);
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    const pokedexImage = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    renderWithRouter(<About />);
+
+    const image = screen.getByAltText('Pokédex').src;
+    expect(image).toEqual(pokedexImage);
+  });
+});
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,14 @@
+// Função retirada do Course - Módulo de Front-end - Bloco 14.3 - RTL - Testando React Router
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>),
+    history,
+  });
+};
+export default renderWithRouter;
